89cc387e4c9e499494695eeb45ae6370c592a322
Aleksandr Pankov <nayteruz@gmail.com>
2022-06-07 17:43:14 +0700

    Initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="btn">Hello</button>
+  <RouterView/>
+</template>
+
+<script>
+import {RouterView} from "vue-router";
+
+export default {
+  setup() {
+
+  },
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import "materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+
+$roboto:'Roboto', sans-serif;
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>
+      Hello Vue3
+      <p>test test</p>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="scss">
+  h1 {
+    margin: 0;
+    p {
+      padding-top: 10px;
+    }
+  }
+</style>
